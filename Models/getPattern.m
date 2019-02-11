753f0b7a3d70dfaf7d867f4157c40c14b78aa4bb
kforthman <katie.forthman@gmail.com>
2019-02-11 12:12:29 -0600

    Get Pattern
    Add file getPattern.m that denotes the pattern for each trial and each
    participant. Add resulting files blockPatterns.csv and
    blockPatterns.txt.

--- Models/getPattern.m
+++ Models/getPattern.m
@@ -1,7 +1,5 @@
-% Format data from test version 2.2.1 of chicken task to be read into
-% fitAdaptivityModel.m (developed by Glaze et al.)
-%
-% Created by Katherine Forthman, 09.17.2018
+
+
 
 % Designate the file in which the data is located
 pathFrom = '/Volumes/T1000/BehavioralTasks/MTURK/ChickenTask/data/tasks/data_for_katie/';
@@ -28,6 +26,7 @@ end
 % List all the subjects with data in the raw data directory
 subjids = unique(file_details(:,4));
 
+trial = {'T1','T2'};
 
 % Do the subjects have data for session 1?
 T1 = cell(length(subjids),1);
@@ -40,41 +39,50 @@ T2 = cell(length(subjids),1);
 for i = 1:length(subjids)
     T2{i,1} = ismember(array2table([subjids(i), 'T2']), array2table(file_details(:,4:5)), 'rows');
 end
-
 % A table of each subject and their sessions completed.
 availableData = cell2table([subjids, T1, T2]);
 
-save([pathTo '/Analysis/subj_info.mat'], 'subjids')
-
-% For each subject, create the data structure. The data structure from each
-% session is combined into a single file for the subject and saved in the
-% output directory. If both sessions are not available, no file is created.
+k=1;
 for i = 1:length(subjids)
     
     disp(i)
     
     if availableData{i,2} && availableData{i,3}
-        filename = [pathFrom 'wave2-CT-' subjids{i} '-T1.csv'];
-        dataT1 = createStruct(filename);
-        
-        for j = 1:6
-            js = num2str(j);
-            eval(['dataT1B' js '= dataT1{' js '}'])
+        for j = 1:2
+            filename = [pathFrom 'wave2-CT-' subjids{i} '-' trial{j} '.csv'];
+            
+            fid = fopen(filename);
+            header = strsplit(fgetl(fid), ',');
+            %colNames = strsplit(fgetl(fid), ',');
+            fclose(fid);
+            
+            ua_pos = find(strcmp(header, 'UserAGENT'));
+            useragent = strjoin(header((ua_pos+1):end), ',');
+            header = header(1:(ua_pos+1));
+            header((ua_pos+1)) = cellstr(useragent);
+            
+            header = reshape(header, 2, []);
+            header = cell2struct(header(2,:), header(1,:), 2);
+            
+            data = readtable(filename, 'ReadVariableNames', true, 'Delimiter', ',', 'HeaderLines', 1);
+            
+            % ID
+            splt = strsplit(header.Orginal_File_Name, {'-','.'});
+            id{k} = splt{2};
+            
+            % Pattern
+            pattern{k} = header.Pattern;
+            
+            t{k} = trial{j};
+            
+            k = k + 1;
         end
-        
-        filename = [pathFrom 'wave2-CT-' subjids{i} '-T2.csv'];
-        dataT2 = createStruct(filename);
-        
-        for j = 1:6
-            js = num2str(j);
-            eval(['dataT2B' js '= dataT2{' js '}']);
-        end
-        
-        save([pathTo '/Raw/data_' subjids{i} '.mat'], ...
-            'dataT1B1', 'dataT1B2', 'dataT1B3', 'dataT1B4', 'dataT1B5', 'dataT1B6', ...
-            'dataT2B1', 'dataT2B2', 'dataT2B3', 'dataT2B4', 'dataT2B5', 'dataT2B6')
-    else
-        continue
     end
-    
 end
+
+id = id.';
+t = t.';
+pattern = pattern.';
+
+myTable = table(id,t,pattern);
+writetable(myTable, fullfile(pathTo, 'blockPatterns.csv'))
