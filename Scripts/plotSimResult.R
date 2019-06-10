cd3130b572847852e5bff18dc6718bed8dcbebdd
Katie Forthman <katie.forthman@gmail.com>
2019-06-10 16:37:08 -0500

    Minor plot changes
    Minor changes to the way the simulation results are plotted.

--- Scripts/plotSimResult.R
+++ Scripts/plotSimResult.R
@@ -20,27 +20,29 @@ for(i in 1:length(simTypes)){
 
 simAvg_df <- as.data.frame(simAvg)
 
-for(this_sigma in c(30,110,160)){
+for(this_sigma in c(110,160)){
   for(this_Htrue in c(0.05, 0.95)){
-    ggp <- ggplot(subset(simAvg_df, N == 150 & H_true == this_Htrue & sigma == this_sigma), aes(x=H_subj, y=H_subj_EST_avg)) + 
-      geom_errorbar(aes(ymin=H_subj_EST_avg-H_subj_EST_stder, ymax=H_subj_EST_avg+H_subj_EST_stder), width=.1) +
-      geom_point()+
-      ylim(0,1)+
-      geom_abline(intercept = 0)+
-      ggtitle(paste0("H_true = ", this_Htrue, " & sigma = ", this_sigma))
-    print(ggp)
+    for(this_N in c(50,100,150)){
+      ggp <- ggplot(subset(simAvg_df, N == this_N & H_true == this_Htrue & sigma == this_sigma), aes(x=H_subj, y=H_subj_EST_avg)) + 
+        geom_errorbar(aes(ymin=H_subj_EST_avg-H_subj_EST_stder, ymax=H_subj_EST_avg+H_subj_EST_stder), width=.1) +
+        geom_point()+
+        ylim(0,1)+
+        geom_abline(intercept = 0)+
+        ggtitle(paste0("H_true = ", this_Htrue, " & sigma = ", this_sigma, " & N = ", this_N))
+      print(ggp)
+    }
   }
 }
 
 
-for(this_sigma in c(30,110,160)){
-  for(this_Htrue in c(0.05, 0.95)){
-    ggp <- ggplot(subset(simAvg_df, N == 150 & H_true == this_Htrue & sigma == this_sigma), aes(x=H_subj, y=pct_avg)) + 
-      geom_errorbar(aes(ymin=pct_avg-pct_stder, ymax=pct_avg+pct_stder), width=.1) +
-      geom_point()+
-      ylim(0,1)+
-      geom_abline(intercept = 0)+
-      ggtitle(paste0("PCT: H_true = ", this_Htrue, " & sigma = ", this_sigma))
-    print(ggp)
-  }
-}
+# for(this_sigma in c(30,110,160)){
+#   for(this_Htrue in c(0.05, 0.95)){
+#     ggp <- ggplot(subset(simAvg_df, N == 150 & H_true == this_Htrue & sigma == this_sigma), aes(x=H_subj, y=pct_avg)) + 
+#       geom_errorbar(aes(ymin=pct_avg-pct_stder, ymax=pct_avg+pct_stder), width=.1) +
+#       geom_point()+
+#       ylim(0,1)+
+#       geom_abline(intercept = 0)+
+#       ggtitle(paste0("PCT: H_true = ", this_Htrue, " & sigma = ", this_sigma))
+#     print(ggp)
+#   }
+# }
